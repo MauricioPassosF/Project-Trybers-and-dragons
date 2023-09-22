046c388580fb12a7ee70c0d10380265a72b65c01
MauricioPassosF <mauricio.passos97@gmail.com>
2023-09-22 13:34:43 -0300

    Req 1:ok

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,12 @@
+// import Race from './Race';
+
+// export default class Dwarf extends Race {
+//   constructor(name:string, dexterity: number) {
+//     super(name, dexterity);
+//     this.maxLifePoints = 80;
+//   }
+
+//   get maxLifePoints(): number {
+//     return this.maxLifePoints;
+//   }
+// }
